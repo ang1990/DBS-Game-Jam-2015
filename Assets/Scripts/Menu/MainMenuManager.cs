1773b5d516d518f3a792e967480b9172671f8d30
renaldy <renaldy_widjaja@yahoo.com>
2015-12-12 19:55:53 +0800

    Title scene hooked up

--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -3,23 +3,13 @@ using System.Collections;
 
 public class MainMenuManager : MonoBehaviour {
 
-    public GameObject MainMenuObject;
-
 	// Use this for initialization
 	void Start () {
-        MainMenuObject = GameObject.Find("MainMenuObject");
-        MainMenuObject.SetActive(true);
 	}
 
     public void PlayButton()
     {
         Application.LoadLevel("InGame");
-    }
-		
-    public void BackButton(GameObject UIObject)
-    {
-        UIObject.SetActive(false);
-        MainMenuObject.SetActive(true);
     }
 
     public void CreditButton()
