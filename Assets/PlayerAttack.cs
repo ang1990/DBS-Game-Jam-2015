f170d6f846379a47e24a139a18e4af12bc419c02
ang1990 <ang1990@hotmail.com>
2015-12-12 11:53:43 +0800

    Player Attack Stub.

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+	public AudioClip swoosh;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
