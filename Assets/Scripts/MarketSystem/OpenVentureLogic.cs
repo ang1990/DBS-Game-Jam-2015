315948b562b37a297fdbdf48a98d2719d678e9fb
Tan Tack Poh <colonel.tan911220@gmail.com>
2015-12-12 14:30:56 +0800

    Market Logic in place

---
+++ Assets/Scripts/MarketSystem/OpenVentureLogic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenVentureLogic : MonoBehaviour {
+
+	uint capital = 0;
+	float profitPercentage = 0;
+	PlayerData playerData;
+	OverallMarketLogic overallMarketLogic;
+
+	// Use this for initialization
+	void Awake () {
+		playerData = GameObject.Find("GameManager").GetComponent<PlayerData> ();
+		overallMarketLogic = GameObject.Find ("GameManager").GetComponent<OverallMarketLogic> ();
+		InvokeRepeating ("GenerateProfit", overallMarketLogic.timerSec, overallMarketLogic.timerSec);
+	}
+	
+	// Update is called once per frame
+	uint GenerateProfit () {
+		profitPercentage = Random.Range (1.01, 1.05);
+		uint newCapital = capital * profitPercentage;
+		return newCapital - capital;
+	}
+}
---
+++ Assets/Scripts/MarketSystem/StockMarketLogic.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StockMarketLogic : MonoBehaviour {
+
+	uint stockPricePerUnit = 0;
+	uint currentUnit = 0;
+	uint buyingUnit = 0;
+	uint sellingUnit = 0; 
+	uint cost = 0;
+	uint worth = 0;
+	uint profitLossDifference = worth - cost;
+
+	// Use this for initialization
+	void Awake () {
+		stockPricePerUnit = 0;
+		currentUnit = 0;
+		buyingUnit = 100;
+		sellingUnit = currentUnit; 
+		cost = 0;
+		worth = currentUnit * stockPricePerUnit;
+		profitLossDifference = worth - cost;
+	}
+	
+	// Update is called once per frame
+	void updateCurrentUnit (uint unitCount) {
+		currentUnit = unitCount;
+		sellingUnit = currentUnit;
+	}
+
+	void updateCost (uint unitCount) {
+		currentUnit = unitCount;
+		sellingUnit = currentUnit;
+	}
+}
