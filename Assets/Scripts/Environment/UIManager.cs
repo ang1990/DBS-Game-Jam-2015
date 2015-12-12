ad7b6d7b8e5155ed949df5fd48708df1612daf5a
Tan Tack Poh <colonel.tan911220@gmail.com>
2015-12-12 20:00:20 +0800

    Stock UI Change

--- Assets/Scripts/Environment/UIManager.cs
+++ Assets/Scripts/Environment/UIManager.cs
@@ -4,29 +4,50 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
-	Text stockText;
 	Text cashText;
-    Text stockChangeIncrementText;
-    Text stockChangeDecrementText;
-    public uint stockChange = 50000;
+	Text averageCostText;
+	Text currentStockPriceText;
+	Text currentStockUnitText;
+	Text costsText;
+	Text worthText;
+	Text profitLossText;
+
     PlayerData playerData;
+	StockMarketLogic stockMarketLogic;
 
 	// Use this for initialization
 	void Start () {
-		stockText = GameObject.Find ("StockDisplay").GetComponent<Text> ();
 		cashText = GameObject.Find ("MoneyDispText").GetComponent<Text> ();
-        stockChangeIncrementText = GameObject.Find("AddStock").GetComponentInChildren<Text> ();
-        stockChangeDecrementText = GameObject.Find("MinusStock").GetComponentInChildren<Text>();
-        playerData = GameObject.Find("GameManager").GetComponent<PlayerData> ();
+		averageCostText = GameObject.Find ("AverageCostDisplay").GetComponent<Text> ();
+		currentStockPriceText = GameObject.Find ("StockDisplay").GetComponent<Text> ();
+		costsText = GameObject.Find ("CostDisplay").GetComponent<Text> ();
+		worthText = GameObject.Find ("WorthDisplay").GetComponent<Text> ();
+		profitLossText = GameObject.Find ("ProfitLossDisplay").GetComponent<Text> ();
+
+		playerData = GameObject.Find ("GameManager").GetComponent<PlayerData> ();
+		stockMarketLogic = GameObject.Find ("GameManager").GetComponent<StockMarketLogic> ();
 
         cashText.text = "$" + playerData.cash.ToString();
-        stockText.text = "$" + playerData.stock.ToString();
-        stockChangeIncrementText.text = "+$" + stockChange.ToString();
-        stockChangeDecrementText.text = "-$" + stockChange.ToString();
+		averageCostText.text = "$" + stockMarketLogic.averageCost.ToString();
+		currentStockPriceText.text = "$" + stockMarketLogic.currentStockPricePerUnit.ToString ();
+		currentStockUnitText.text = stockMarketLogic.currentUnit.ToString ();
+		costsText.text = stockMarketLogic.cost.ToString ();
+		worthText.text = stockMarketLogic.worth.ToString ();
+		profitLossText.text = stockMarketLogic.profitLossDifference.ToString ();
 	}
 
-	public void UpdateStockText(uint value) {
-		stockText.text = "$" + value.ToString ();
+	public void UpdateStockMarketText(	uint averageCostVal, 
+										uint currentStockPriceVal,
+										uint currentUnitVal, 
+										uint costsVal, 
+										uint worthVal, 
+										uint profitLossVal) {
+		averageCostText.text = "$" + averageCostVal.ToString();
+		currentStockPriceText.text = "$" + currentStockPriceVal.ToString ();
+		currentStockUnitText.text = currentUnitVal.ToString ();
+		costsText.text = costsVal.ToString ();
+		worthText.text = worthVal.ToString ();
+		profitLossText.text = profitLossVal.ToString ();
 	}
 
 	public void UpdateCashText(uint value) {
