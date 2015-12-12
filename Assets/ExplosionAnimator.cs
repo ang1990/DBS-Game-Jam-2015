35a1fcf881bfb1227690fc11cc662dc4c31c72c6
ang1990 <ang1990@hotmail.com>
2015-12-12 18:54:09 +0800

    Rage Sprite updated, explosion implemented.

--- Assets/ExplosionAnimator.cs
+++ Assets/ExplosionAnimator.cs
@@ -3,15 +3,31 @@ using System.Collections;
 
 public class ExplosionAnimator : MonoBehaviour {
 
-	public Sprite sprites[];
+	public Sprite[] sprites;
+	float refreshTime = 0.05f;
+	float lastRefreshTime;
+	int currentFrame;
+	SpriteRenderer sr;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		sr = GetComponent<SpriteRenderer> ();
+		currentFrame = 0;
+		lastRefreshTime = Time.timeSinceLevelLoad;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Time.timeSinceLevelLoad > lastRefreshTime + refreshTime) {
+			Debug.Log ("Current Frame: " + currentFrame.ToString ());
+			lastRefreshTime = Time.timeSinceLevelLoad;
+			currentFrame++;
+			if (currentFrame >= 4)
+				Destroy (gameObject);
+			else {
+				sr.enabled = true;
+				sr.sprite = sprites [currentFrame];
+			}
+		}
 	}
 }
