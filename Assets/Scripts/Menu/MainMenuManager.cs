28381aa69c649b805ccb575cfdfe6ec4cdddfa86
renaldy <renaldy_widjaja@yahoo.com>
2015-12-12 17:41:38 +0800

    Rage png is cropped

--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -4,15 +4,14 @@ using System.Collections;
 public class MainMenuManager : MonoBehaviour {
 
     public GameObject MainMenuObject;
-    public GameObject InstructionsObject;
     public GameObject CreditsObject;
 
 	// Use this for initialization
 	void Start () {
         MainMenuObject = GameObject.Find("MainMenuObject");
         MainMenuObject.SetActive(true);
-        InstructionsObject = GameObject.Find("InstructionsObject");
-        InstructionsObject.SetActive(false);
+
+
         CreditsObject = GameObject.Find("CreditObject");
         CreditsObject.SetActive(false);
 	}
@@ -20,7 +19,48 @@ public class MainMenuManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        
+        //if (StoryObject.activeSelf)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.E))
+        //    {
+        //        StoryObject.SetActive(false);
+        //        MainMenuObject.SetActive(true);
+        //    }
+        //}
+        //else if (MainMenuObject.activeSelf)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.S))
+        //    {
+        //        Application.LoadLevel(1);
+        //    }
+        //    else if (Input.GetKeyDown(KeyCode.H))
+        //    {
+        //        InstructionsObject.SetActive(true);
+        //        MainMenuObject.SetActive(false);
+        //    }
+        //    else if (Input.GetKeyDown(KeyCode.C))
+        //    {
+        //        CreditsObject.SetActive(true);
+        //        MainMenuObject.SetActive(false);
+        //    }
+        //    else if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Escape))
+        //    {
+        //        Application.Quit();
+        //    }
+        //}
+        //else if (InstructionsObject.activeSelf || CreditsObject.activeSelf)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.Escape))
+        //    {
+        //        InstructionsObject.SetActive(false);
+        //        CreditsObject.SetActive(false);
+        //        MainMenuObject.SetActive(true);
+        //    }
+        //    else if (Input.GetKeyDown(KeyCode.S))
+        //    {
+        //        Application.LoadLevel(1);
+        //    }
+        //}
 
 	}
 
@@ -28,13 +68,7 @@ public class MainMenuManager : MonoBehaviour {
     {
         Application.LoadLevel(index);
     }
-
-    public void InstructionButton(GameObject UIObject)
-    {
-        UIObject.SetActive(true);
-        MainMenuObject.SetActive(false);
-    }
-
+		
     public void BackButton(GameObject UIObject)
     {
         UIObject.SetActive(false);
